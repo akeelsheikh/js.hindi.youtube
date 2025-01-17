d4d1323cd8dc79628248021eea0e72465085c5b7
Akeel Ahmad <akeelcare@gmail.com>
2025-01-17 05:37:59 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 12345
+let accountEmail = "akeelcare@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+// console.log(accountCity);
+console.table([ accountId , accountEmail , accountPassword, accountCity,accountState]);
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // all codes are used as newer version of js
+
+let name = "Akeel"
+let age = 18
+let isLoggedIn = true
+
+//numbers -> 2 to power 53
+// bigint
+//string -> ""
+//boolean -> T/F
+//null -> standalone value
+//undefined ->
+//symbol -> unique
+//object
+
+console.log(typeof "akeel"); // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
