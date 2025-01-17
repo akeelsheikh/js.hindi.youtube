cf1cfd4d2f5cc0ae4f5d0bcebf5bb7c3d6938484
Akeel Ahmad <akeelcare@gmail.com>
2025-01-17 06:15:15 +0000

    conversionOpration

---
+++ 01_basics/03_conversionOpration.js
@@ -0,0 +1,23 @@
+//let score = true
+
+//console.log(typeof score); // = 33 //number
+
+
+//console.log(typeof score); // = "33" //string
+//let valueInNumbers = Number(score); // number //conversion
+//console.log(typeof valueInNumbers); // number
+//console.log(valueInNumbers); //true // 1
+
+
+//"33" -> 33
+//"33abc" => NaN
+//true => 1 / false => 0
+
+//1 ->true // 0-> false
+//"" ->false
+//"akeel" -> true
+
+let aNumber = 33
+let stringNumber = String(aNumber)
+console.log( typeof stringNumber);
+console.log(  stringNumber);
